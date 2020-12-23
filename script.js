cfc36a626fa1148a5444ca83ef52415a3a6a9df8
isurojit <iamsurojit@hotmail.com>
2020-12-23 18:44:41 +0530

    errors

--- script.js
+++ script.js
@@ -7,9 +7,23 @@ function claculateResult(e){
     const UIfahrenheit = document.querySelector('#fahrenheit');
     const UIconverter = document.querySelector('#converter');
 
-    UIconverter.addEventListener("submit", function(e){
-        console.log(e.value);
-    });
+    UIconverter.addEventListener("change", claculateConverter);
 
     e.preventDefault();
+}
+function claculateConverter(){
+    const converterValue =UIconverter[UIconverter.selectedIndex].value;
+    //Getting result UI
+    const result = document.querySelector('#result');
+    const celsiusValue =((UIfahrenheit-32)*5/9);
+    const kelvinValue =((UIfahrenheit-32)*5/9)+273.15;
+    const rankineValue = (UIfahrenheit+459.67);
+    console.log(celsiusValue);
+    if(converterValue == "celsius"){
+        result.appendChild(document.createTextNode(celsiusValue));
+    }if(converterValue == "kelvin"){
+        result.appendChild(document.createTextNode(kelvinValue));
+    }if(converterValue == "rankine"){
+        result.appendChild(document.createTextNode(rankineValue));
+    }
 }
